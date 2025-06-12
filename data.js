04b202bdc80c5e220a08adc5d531d8f75e8fdc32
E-RKP <edobackup111@gmail.com>
2025-06-12 12:42:07 +0200

    version 4

--- data.js
+++ data.js
@@ -2,4 +2,11 @@ const button  = document.getElementById('button')
 
 button.addEventListener('click', ()=>{
   alert('Hai cliccato!')
+
+  let pEl = document.createElement('p')
+  pEl.textContent = 'aoooooo'
+  document.body.appendChild(pEl)
 })
+
+let response = fetch('nivniv')
+console.log(response)
